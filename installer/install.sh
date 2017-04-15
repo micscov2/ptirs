f82ad3462ca1e2dd5038f4445825c2731f225bba
micscov2 <micscov2@gmail.com>
2017-04-15 05:32:31 -0700

    Added mongodb installation

--- installer/install.sh
+++ installer/install.sh
@@ -4,11 +4,17 @@
 # This flag will enable debug information for flask
 export FLASK_DEBUG=1
 
-# Installing system dependencies
-# TODO: Install mongodb
-sudo apt-get update >> debug.log
-sudo apt-get install python-pip >> debug.log
+# Installing MongoDB
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
 
+# TODO: Check if MongoDB is running
+
+# Installing system dependencies
+sudo apt-get update >> ../log/debug.log
+sudo apt-get install python-pip >> ../log/debug.log
 
 # Install python dependencies
-sudo pip install -r requirements.txt >> debug.log
+sudo pip install -r requirements.txt >> ../log/debug.log
